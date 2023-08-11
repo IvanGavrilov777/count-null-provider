6ffc118290eaf1bc2fa2c4444d13e34a110e8c64
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2023-08-11 15:50:25 +0300

    Update count-null-provider.tf

--- count-null-provider.tf
+++ count-null-provider.tf
@@ -2,7 +2,7 @@ variable "condition" {
   default = false
 }
 resource "null_resource" "screen_output" {
-count = var.condition?1 : 20972
+count = var.condition?1 : 3
   provisioner "local-exec" {
     command = <<EOT
     echo 'Hello World'
