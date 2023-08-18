cc73e2af8f61e9edb3efa051b7fe486e782af953
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2023-08-18 14:43:25 +0300

    Update 1.tf

--- runtrigger/1.tf
+++ runtrigger/1.tf
@@ -1,6 +1,9 @@
 #asd1231231231
 resource "null_resource" "screen_output" {
 count = 3
+triggers = {
+value = timestamp ()
+}
   provisioner "local-exec" {
     command = <<EOT
     aws eks get-token
