6a6b1d2e89a800e4808d245bec9836c9e56e6b82
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2023-08-24 17:32:28 +0300

    Update count-null-provider.tf

--- count-null-provider.tf
+++ count-null-provider.tf
@@ -2,6 +2,9 @@ variable "condition" {
   default = false
 }
 resource "null_resource" "screen_output" {
+triggers = {
+time = timestamp ()
+}
 count = var.condition?1 : 3
   provisioner "local-exec" {
     command = <<EOT
