2a47f86b67368097d29c7d7bb18848af25833fc3
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2024-12-19 13:28:23 +0200

    Update count-null-provider.tf

--- count-null-provider.tf
+++ count-null-provider.tf
@@ -1,15 +1,68 @@
 variable "condition" {
   default = false
 }
-resource "null_resource" "screen_output" {
+resource "null_resource" "db2_check_status" {
 #triggers = {
 #time = timestamp ()
 #}
-count = var.condition?1 : 5
+count = var.condition?1 : 2
   provisioner "local-exec" {
     command = <<EOT
    ls
     EOT
   }
+}
+resource "null_resource" "wrm" {
+#triggers = {
+#time = timestamp ()
+#}
+count = var.condition?1 : 2
+  provisioner "local-exec" {
+    command = <<EOT
+   ls
+    EOT
+  }
+depends_on = [null_resource.ystar]
+}
+
+resource "null_resource" "wsm" {
+#triggers = {
+#time = timestamp ()
+#}
+count = var.condition?1 : 2
+  provisioner "local-exec" {
+    command = <<EOT
+   ls
+    EOT
+  }
+depends_on = [null_resource.db2_check_status]
+}
+resource "null_resource" "ystar" {
+#triggers = {
+#time = timestamp ()
+#}
+count = var.condition?1 : 2
+  provisioner "local-exec" {
+    command = <<EOT
+   ls
+    EOT
+  }
+depends_on = [null_resource.wsm]
+}
+
+
+depends_on = [null_resource.db2_check_status]
+}
+resource "null_resource" "volume_creation" {
+#triggers = {
+#time = timestamp ()
+#}
+count = var.condition?1 : 2
+  provisioner "local-exec" {
+    command = <<EOT
+   ls
+    EOT
+  }
+depends_on = [null_resource.wrm]
 }
 #######12312313231123123123123123112123123
